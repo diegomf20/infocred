3ed921efb29cd3096db4611c5634a43ac7f7b46e
Diego Mendoza <diegomf.mendoza@gmail.com>
2019-08-10 00:39:42 -0500

    terminado

--- mail.php
+++ mail.php
@@ -9,20 +9,19 @@ require 'vendor/autoload.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
-
 try {
     //Server settings
     $mail->SMTPDebug = 0;                                       // Enable verbose debug output
     $mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = 'mail.fishti.com';  // Specify main and backup SMTP servers
+    $mail->Host       = 'mail.infocred.pe';  // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'info@fishti.com';                     // SMTP username
-    $mail->Password   = 'stunsd18@';                               // SMTP password
+    $mail->Username   = 'sistemas@infocred.pe';                     // SMTP username
+    $mail->Password   = 'chiclayo2019';                               // SMTP password
     $mail->SMTPSecure = 'SMTP';                                  // Enable TLS encryption, `ssl` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
+    $mail->Port       = 2525;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('info@fishti.com', 'Fishti Info');
+    $mail->setFrom('sistemas@infocred.pe', 'Sistemas Infocred');
     $mail->addAddress('diegomf.mendoza@gmail.com');     // Add a recipient
     // $mail->addAddress('ellen@example.com');               // Name is optional
     // $mail->addReplyTo('info@example.com', 'Information');
@@ -35,9 +34,13 @@ try {
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'Formulario Landing Page';
+    $mail->Body    = '<b>Nombre:</b> '.$_REQUEST['nombre'].'<br>';
+    $mail->Body    = $mail->Body.'<b>Empresa:</b> '.$_REQUEST['empresa'].'<br>';
+    $mail->Body    = $mail->Body.'<b>Telefono:</b> '.$_REQUEST['telefono'].'<br>';
+    $mail->Body    = $mail->Body.'<b>Email:</b> '.$_REQUEST['mail'].'<br>';
+    $mail->Body    = $mail->Body.'<b>Descripcion:</b><br>'.$_REQUEST['descripcion'].'<br>';
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     header('Content-Type: application/json');    
     if (!$mail->send()) {
